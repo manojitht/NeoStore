77a2287b0f71f653ae08c0ba2abd10b867b41bb9
manojitht <manojith12345@outlook.com>
2025-03-08 23:36:51 +0530

    feat-backend: jwt token, schemas added

---
+++ src/models/local_db.py
@@ -0,0 +1,9 @@
+db = {
+    "manojith": {
+        "username": "manojith",
+        "fullname": "Manojith Thiyageswarakumar",
+        "email": "manojith@gmail.com",
+        "hashed_password": "",
+        "disabled": False
+    }
+}
